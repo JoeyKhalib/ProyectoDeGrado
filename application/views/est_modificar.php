80f7e0fa416c114954be944e628ba6d2831f383c
PabloQ <pabloquintanillaxd@gmail.com>
2021-07-06 18:06:44 -0400

    Creando modificar,formulario,menu y test en las vistas

---
+++ application/views/est_modificar.php
@@ -0,0 +1,47 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+ 
+<?php 
+foreach ($infousuario->result() as $row) 	
+ {
+ 	echo form_open_multipart('usuario/modificarbd');
+ ?>
+
+<input type="hidden" name="idUsuario" value="<?php echo $row->idUsuario;?>">
+
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nombre</label>
+    <input type="text" class="form-control" name="nombre" value="<?php echo $row->nombre;?>">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Apellido Paterno</label>
+    <input type="text" class="form-control" name="ApellidoPaterno" value="<?php echo $row->ApellidoPaterno;?>">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Apellido Materno</label>
+    <input type="text" class="form-control" name="ApellidoMaterno" value="<?php echo $row->ApellidoMaterno;?>">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Cedula de Identidad</label>
+    <input type="text" class="form-control" name="Cedula" value="<?php echo $row->carnet;?>">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Liga</label>
+    <input type="text" class="form-control" name="Liga" value="<?php echo $row->liga;?>">
+  </div>
+
+
+  <button type="submit" class="btn btn-primary">Modificar</button>
+
+
+<?php 
+echo form_close(); 
+}
+?>
+
+
+  	</div>
+  </div>
+</div>
